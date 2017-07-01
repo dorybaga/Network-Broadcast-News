1901b5c9e1122fa34e94c14143b174eada6bd883
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-01 13:53:11 -1000

    created new server; server listening on port 6969

---
+++ server.js
@@ -0,0 +1,11 @@
+/*jshint esversion: 6 */
+
+const net = require('net');
+
+// Create new server
+const server = net.createServer();
+
+// Make server listen
+server.listen(6969, '0.0.0.0', function(){
+  console.log('Server is listening on port 6969');
+});
