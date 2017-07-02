0f087f47a9a42e86eab9cff1bb9e4d1bdc6c8afe
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-01 16:36:04 -1000

    client can send data to server

--- client.js
+++ client.js
@@ -3,8 +3,12 @@
 // Connect to server
 
 const net = require('net');
-net.connect(6969, '0.0.0.0', function(){
+const client = net.createConnection(6969, '0.0.0.0', function(){
   console.log('Client is connected to the server!');
 });
 
+// Input data and pipe to server
 
+client.on('connect', function(){
+  process.stdin.pipe(client);
+});
