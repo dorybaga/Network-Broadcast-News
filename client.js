674855df9a912e10462f2102e5b9c26c2ef7feba
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-01 15:06:39 -1000

    client is connected to server

--- client.js
+++ client.js
@@ -1,3 +1,10 @@
 /*jshint esversion: 6 */
 
-const server = require('./server.js');
+// Connect to server
+
+const net = require('net');
+net.connect(6969, '0.0.0.0', function(){
+  console.log('Client is connected to the server!');
+});
+
+
