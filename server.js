307f516b9d6e6009ec92deb19f7b9111a6ba90d6
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-01 18:42:15 -1000

    server relays data to all clients

--- server.js
+++ server.js
@@ -1,10 +1,15 @@
 /*jshint esversion: 6 */
 
 const net = require('net');
+const clients = [];
 
 // Create new server
 const server = net.createServer(function(socket){
   console.log('A new client is connected!');
+
+  // Add client to list
+  clients.push(socket);
+  // console.log(clients);
 
   // Identify client's IP address
   const clientID = socket.remoteAddress;
@@ -12,13 +17,22 @@ const server = net.createServer(function(socket){
 
   // Listens for data; if data is recieved (from client) renders data to terminal
   socket.on('data', function(data){
-    console.log(clientID + ": " + data.toString());
+    process.stdout.write(clientID + ": " + data.toString());
 
+    // Blast message to other clients connected to chat
+    clients.forEach(function(client){
+      client.write(data);
+    });
 
   });
+
+
+
 });
 
 // Make server listen
 server.listen(6969, '0.0.0.0', function(){
   console.log('Server is listening on port 6969');
 });
+
+
