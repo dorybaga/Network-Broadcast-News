e4ec00827409474b807a2d43bc888475d7d62779
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-01 17:08:48 -1000

    oops, no changes

--- client.js
+++ client.js
@@ -1,14 +1,15 @@
 /*jshint esversion: 6 */
 
 // Connect to server
-
 const net = require('net');
 const client = net.createConnection(6969, '0.0.0.0', function(){
   console.log('Client is connected to the server!');
 });
 
 // Input data and pipe to server
-
 client.on('connect', function(){
   process.stdin.pipe(client);
 });
+
+// Client should recieve data from server
+
