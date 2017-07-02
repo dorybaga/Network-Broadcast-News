7ea991b6b2fdbf28684f86306e1054e41b216e8b
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-01 16:36:28 -1000

    server can recieve input data from client

--- server.js
+++ server.js
@@ -4,11 +4,15 @@
 const net = require('net');
 
 // Create new server
-const server = net.createServer();
+const server = net.createServer(function(socket){
+  console.log('A new client is connected!');
+  socket.on('data', function(data){
+    console.log(data.toString());
+  });
+});
 
 // Make server listen
 server.listen(6969, '0.0.0.0', function(){
   console.log('Server is listening on port 6969');
 });
 
-// 'I have a client connected'
