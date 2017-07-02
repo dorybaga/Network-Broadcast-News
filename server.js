7bdd12da0cce43551784ac6b1d7e3caff03e7cb5
Dory Baga <dorybaga@Dorys-MacBook-Pro.local>
2017-07-01 15:06:03 -1000

    server listening for client

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 /*jshint esversion: 6 */
 
+//
 const net = require('net');
 
 // Create new server
@@ -9,3 +10,5 @@ const server = net.createServer();
 server.listen(6969, '0.0.0.0', function(){
   console.log('Server is listening on port 6969');
 });
+
+// 'I have a client connected'
